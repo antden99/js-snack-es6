d25381e506599bff92ea7bc598012fb4f7318b60
antden99 <antoniodenigris@icloud.com>
2024-03-06 11:46:12 +0100

    add snack-1

--- snack 4/js/main.js
+++ snack 4/js/main.js
@@ -2,29 +2,29 @@ console.log("it works");
 
 const listPeople = [
     {
-        name : "Antonio",
-        surname : "De Nigris",
-        age : 25
+        name: "Antonio Franco",
+        surname: "De Nigris",
+        age: 25
     },
     {
-        name : "Francesco",
-        surname : "De Nigris",
-        age : 25
+        name: "Francesco Franco",
+        surname: "De Nigris",
+        age: 25
     },
     {
-        name : "Angelo",
-        surname : "Carola",
-        age : 12
+        name: "Angelo Franco ",
+        surname: "Carola",
+        age: 12
     },
     {
-        name : "Giovanni",
-        surname : "Bertolini",
-        age : 10
+        name: "Giovanni Franco",
+        surname: "Bertolini",
+        age: 10
     },
     {
-        name : "Stefano",
-        surname : "Modica",
-        age : 27
+        name: "Stefano Franco",
+        surname: "Modica",
+        age: 27
     },
 
 ]
@@ -33,7 +33,7 @@ console.log(listPeople);
 
 const newList = listPeople.map((element) => {
 
-    if (element.age >= 18){
+    if (element.age >= 18) {
         return (element.name + " può guidare");
     } else {
         return (element.name + " non può guidare");
@@ -42,3 +42,53 @@ const newList = listPeople.map((element) => {
 })
 
 console.log(newList);
+
+
+const list2 = listPeople.map((element) => {
+
+    const str = element.name.split(" ");
+    console.log(str[0].toLocaleUpperCase() + " " + str[1].toLowerCase());
+})
+
+listPeople.forEach(element => {
+
+    //console.log( element.age)
+    console.log(element["surname"]);
+});
+
+
+const listPeoplPalla = [{
+    nome: "pallina",
+    grandezza: 10,
+    costo: 1000,
+},
+{
+    nome: "pallone",
+    grandezza: 500,
+    costo: 3000,
+},
+
+]
+
+listPeoplPalla.forEach(palla => {
+    
+    for (const key in palla) {
+        console.log(palla[key])
+    }
+    console.log(" ")
+});
+
+
+
+
+const arrayNumb = [1,2,3,4,5,6,];
+
+const newArrayNumb = arrayNumb.filter((number) => {
+
+    if(number % 2 === 0){
+        return number;
+    }
+
+})
+
+console.log(newArrayNumb);
---
+++ snack-1-b5/main.js
@@ -0,0 +1,66 @@
+console.log("tutto ok");
+
+const listZucc = [
+    {
+        varietà: "zucchina 1",
+        peso: 10,
+        lunghezza: "10cm"
+    },
+     {
+        varietà: "zucchina 2",
+        peso: 11,
+        lunghezza: "10cm"
+    },
+     {
+        varietà: "zucchina 3",
+        peso: 15,
+        lunghezza: "10cm"
+    },
+     {
+        varietà: "zucchina 4",
+        peso: 20,
+        lunghezza: "10cm"
+    },
+     {
+        varietà: "zucchina 5",
+        peso: 100,
+        lunghezza: "10cm"
+    },
+     {
+        varietà: "zucchina 6",
+        peso: 8,
+        lunghezza: "10cm"
+    }, 
+    {
+        varietà: "zucchina 7",
+        peso: 6,
+        lunghezza: "10cm"
+    }, 
+    {
+        varietà: "zucchina 8",
+        peso: 70,
+        lunghezza: "10cm"
+    }, 
+    {
+        varietà: "zucchina 9",
+        peso: 3,
+        lunghezza: "10cm"
+    }, 
+    {
+        varietà: "zucchina 10",
+        peso: 20,
+        lunghezza: "10cm"
+    },   
+]
+
+console.log(listZucc);
+
+let sum = 0;
+
+listZucc.forEach((zucchina) =>{
+    console.log(zucchina);
+
+    sum = sum + zucchina.peso;
+})
+
+console.log(sum);
